7bad59b4490b05d263a9501b20373c00fb331532
Ahmad Wali Sayadi <sayadi@Ahmads-MacBook-Pro.local>
2020-10-11 21:31:33 -0700

    plantsList-completed

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,32 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render,screen,fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm/>)
 
-test("form shows success message on submit with form details", () => {});
+
+    const fnameInput = screen.getByLabelText(/first name/i)
+    const lnameInput = screen.getByLabelText(/last name/i)
+
+    fireEvent.change(fnameInput,{target:{value:'ahmad'}})
+    fireEvent.change(lnameInput,{target:{value:'sayadi'}})
+
+  
+ 
+});
+
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm/>)
+
+
+});
+
+
+
+
+
+
+
